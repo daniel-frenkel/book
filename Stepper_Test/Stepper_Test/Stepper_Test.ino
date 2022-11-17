29ca6b0663f2307e22a3caae7b99e88fe42fce11
Daniel Frenkel <daniel.frenkel@gmail.com>
2022-11-17 11:01:49 -0800

    Update Stepper_Test.ino

--- Stepper_Test/Stepper_Test/Stepper_Test.ino
+++ Stepper_Test/Stepper_Test/Stepper_Test.ino
@@ -1,39 +1,19 @@
-// Define pin connections & motor's steps per revolution
-const int dirPin = 2;
-const int stepPin = 3;
-const int stepsPerRevolution = 200;
+const int dirPin = 14;
+const int stepPin = 15;
+const int enPin = 27;
 
 void setup()
 {
-  // Declare pins as Outputs
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
+  pinMode(enPin, OUTPUT);
+  digitalWrite(enPin, LOW);
+  digitalWrite(dirPin, HIGH);
 }
 void loop()
 {
-  // Set motor direction clockwise
-  digitalWrite(dirPin, HIGH);
-
-  // Spin motor slowly
-  for(int x = 0; x < stepsPerRevolution; x++)
-  {
     digitalWrite(stepPin, HIGH);
-    delayMicroseconds(2000);
+    delayMicroseconds(500);
     digitalWrite(stepPin, LOW);
-    delayMicroseconds(2000);
-  }
-  delay(1000); // Wait a second
-  
-  // Set motor direction counterclockwise
-  digitalWrite(dirPin, LOW);
-
-  // Spin motor quickly
-  for(int x = 0; x < stepsPerRevolution; x++)
-  {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(1000);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(1000);
-  }
-  delay(1000); // Wait a second
+    delayMicroseconds(500);
 }
